0363bd8b54246fd4c9e6740e42c72ec7659d5af7
Marlon Kerth <marlonkerth@icloud.com>
2020-11-09 23:47:45 +0100

    Update v2.0 [project page finished] - completly responsible (tablet added last) - only show article boxes when clicked in nav - adjusted article logo images

--- src/script/script.js
+++ src/script/script.js
@@ -1,25 +1,33 @@
 
+// projects show specified article box on associated clicked box and hide rest
+function projectArticleClick(wrapperId) {
+    $('.project-wrapper').each(function () {
+        let id = $(this).attr('id');
+        if(id !== wrapperId) {
+            $(this).hide();
+        } else {
+            $('#'+wrapperId).show();
+        }
+    })
+}
+
 $(document).ready(function () {
     //first initiation add class to first nav elem
     if($('.nav-item').find('.underline--hover').length === 0) {
         let firstItem = $('.nav-item').first();
         firstItem.addClass("nav-item--hover");
-        firstItem.find('.underline').addClass("underline--hover");
         //add background that belongs to current hover
         $('.main-banner-container:nth-child(1)').addClass("main-banner-image--active");
     }
         $('.nav-item').hover(function () {
-            //- remove active hover effects with a for loop -
+            //- remove active hover effects with a for-each loop -
             $('.nav-item--hover').each(function() {
                 $(this).removeClass("nav-item--hover");
-                $(this).find('.underline').removeClass("underline--hover");
                 //get data value of current activ elem and parse it into an integer
                 let activeElemDataValueREMOVE = parseInt($(this).data("value"));
                 //get nth child of banner class using the just retrieved data value and remove the active class
                 $('.main-banner-container:nth-child('+activeElemDataValueREMOVE+')').removeClass("main-banner-image--active");
             });
-            //- add hover effect to current hover -
-            $(this).find('.underline').addClass("underline--hover");
             $(this).addClass("nav-item--hover");
             //get data value of the just hoverdd elem and parse it into an integer
             let activeElemDataValueADD = parseInt($(this).data("value"));
@@ -27,12 +35,13 @@ $(document).ready(function () {
             $('.main-banner-container:nth-child('+activeElemDataValueADD+')').addClass("main-banner-image--active");
         });
 
+    // title ini
     // -- landing page --
     //initiate title animation after some delay
     if(window.location.href.indexOf("index.html") > -1) {
         console.log("landing page initiated"); //debugging purpose
         setTimeout(function () {
-            let typeWritter = new TypeWritter(["people", "life", "love", "project"], '#typewriterText', '#header-underline',true);
+            let typeWritter = new TypeWritter(["people", "life", "love", "project"], '#typewriterText', '#header-underline',false);
             typeWritter.typeWriter().delay(10000);
         }, 600); //600
     }
